b5122de228cb8daac56f51388e6e57a5050ec04a
artorrias <sam.henson1119@outlook.com>
2023-07-17 21:36:18 -0600

    added comments

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Circle = require("./lib/circle.js");
 const Square = require("./lib/square.js");
 const Triangle = require("./lib/triangle.js");
 
-let questions = ["Enter three characters", "Enter a color or hexcode for the text", "Enter a color or hexcode", "Select a shape"];
+let questions = ["Enter three characters", "Enter a color or hexcode for the text", "Enter a color or hexcode", "Select a shape"]; //array of questions to the user
 
 function init() {
     inquirer
@@ -13,7 +13,7 @@ function init() {
             type: 'input',
             message: questions[0],
             name: 'text',
-            validate: (text) => {
+            validate: (text) => { //checks if input is longer than 3 characters, if it is tell them no more than 3
                 if (text.length > 3) {
                     return "please enter no more than 3 characters";
                 }
@@ -38,7 +38,7 @@ function init() {
         }
     ])
     .then(response => {
-        if (response.shape === "triangle") {
+        if (response.shape === "triangle") { //if triangle selected generates a triangle object to make svg
             let logoText = response.text.toUpperCase();
             let logo = new Triangle(response.logocolor, response.textcolor, logoText);
             let svg = logo.render();
@@ -50,7 +50,7 @@ function init() {
                 }
             });
         }
-        else if (response.shape === "circle") {
+        else if (response.shape === "circle") { //if circle selected generates a circle object to make svg
             let logoText = response.text.toUpperCase();
             let logo = new Circle(response.logocolor, response.textcolor, logoText);
             let svg = logo.render();
@@ -62,7 +62,7 @@ function init() {
                 }
             });
         }
-        else {
+        else { //final option is square so square is generated
             let logoText = response.text.toUpperCase();
             let logo = new Square(response.logocolor, response.textcolor, logoText);
             const svg = logo.render();
